65de93a582bb9de6287296f0e9795db989917372
Dr Nic Williams <drnicwilliams@gmail.com>
2015-04-16 09:50:29 -0500

    script to update buildkite-agent blob

---
+++ bin/update_agent_blob.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+agent_path=$1; shift
+if [[ "${agent_path}X" == "X" ]]; then
+  echo "USAGE ./bin/update_agent_blob.sh path/to/buildkite-agent-linux-amd64-1.0-beta.23.450.tar.gz"
+  exit 1
+fi
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+pushd $DIR/..
+  rm -rf blobs/buildkite-agent
+  mkdir -p blobs/buildkite-agent
+  cp ${agent_path} blobs/buildkite-agent
+popd
